ab5d17b7a027cd30bd1c2acfb1885229e7d34aa6
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-04-26 18:32:57 +0400

    Create HeadLineCard.tsx

---
+++ src/components/HeadLineCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeadLineCard = () => {
+  return (
+    <div>
+      {/* Card */}
+      <div>
+        {/* Overlay */}
+        <div>
+          <p>Sun's Out, BOGO's out</p>
+          <p>Trough 8/26</p>
+          <button>Order Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeadLineCard;
